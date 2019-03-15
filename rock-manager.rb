3323efba3df8de658f8bf3bc6ddf833e72bc2320
Bradford Dabbs <brad@perched.io>
2019-03-15 16:02:49 -0500

    Cleanup tests

--- rock-manager.rb
+++ rock-manager.rb
@@ -30,18 +30,9 @@ class RockManager < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test rock-manager`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     head "https://github.com/rocknsm/rock.git", :branch => "devel"
 
-    cmd = "rock help"
-    assert_match "Usage: /usr/local/Cellar/rock-manager/0.1/rock", shell_output(cmd)
+    system bin/"rock", "help"
+    system bin/"rock", "genconfig"
   end
 end
